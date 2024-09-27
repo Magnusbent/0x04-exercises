f449c63fc3e686065689d19c0c0f13e13660d201
Magnusbent <magnusbentlarsen@gmail.com>
2024-09-27 10:55:38 +0200

    Update main.cpp

--- src/exercise-0x00/main.cpp
+++ src/exercise-0x00/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
-int main() {
+using namespace std;
   std::cout << "Exercise 0x00" << std::endl;
+  int findmax(int a, int b, int c,){
+  if (a>=b && a>=c)
+  return a; 
+  if(b>=a && b>=c)
+  return b;
+  else 
+    return c;}
+int main() {
+    int x = 2, y = 4, z = 6;
+  int maxværdi=findmax(x,y,z);
+  cout<<"Den største værdi er: "<< maxværdi << endl;
   return 0;
 }
